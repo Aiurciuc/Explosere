11d35fc257771245d6a7d3c8c6de75fa66978631
Andrei Iuricuc <aiurciuc@gmail.com>
2018-05-05 15:16:29 +0300

    Load image

--- app/src/main/java/com/example/exposere/exposure/MainActivity.java
+++ app/src/main/java/com/example/exposere/exposure/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.exposere.exposure;
 
+import android.Manifest;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -13,9 +15,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
+import pub.devrel.easypermissions.EasyPermissions;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView selectedImage;
+    private static int RESULT_LOAD_IMAGE = 1;
+    private String[] galleryPermissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,32 +30,48 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton addPicButton =  findViewById(R.id.addPic);
+        if (EasyPermissions.hasPermissions(this, galleryPermissions)) {
+        }
+        else {
+            EasyPermissions.requestPermissions(this, "Access for storage",
+                    101, galleryPermissions);
+        }
         addPicButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, 1);
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
             }
+
         });
     }
 
+    //Load image
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK) {
-            Uri photoUri = data.getData();
-            if (photoUri != null) {
-                try {
-                    Bitmap captureBmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(),photoUri );
-                    selectedImage.setImageBitmap(captureBmp);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView image = findViewById(R.id.imageView);
+            image.setImageBitmap(BitmapFactory.decodeFile(picturePath));
         }
+
     }
 
     @Override
